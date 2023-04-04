3d6993de88fb2ef5f9a4f2707947d0cb9355a325
- Vini Mendes <vncmendes>
2023-04-03 23:39:36 -0300

    increasing some features

--- src/server.js
+++ src/server.js
@@ -1,56 +1,44 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-// - Criar Usuários
-// - Listagem de Usuários
-// - Edição de Usuários
-// - Remoção de Usuários
+//  {
+// // - Criar Usuários
+// // - Listagem de Usuários
+// // - Edição de Usuários
+// // - Remoção de Usuários
 
-// - HTTP
-//   - METHOD
-//   - URL
+// // - HTTP
+// //   - METHOD
+// //   - URL
 
-// - GET, POST, PUT, PATCH, DELETE
+// // - GET, POST, PUT, PATCH, DELETE
 
-// GET => Busca um recurso no back-end.
-// POST => Cria um recurso no back-end.
-// PUT => Atualiza um ou mais recursos no back-end, como um formulário.
-// PATCH => Atualiza uma informação específica em um recurso no back-end.
-// DELETE => Remove um recusos do back-end.
+// // GET => Busca um recurso no back-end.
+// // POST => Cria um recurso no back-end.
+// // PUT => Atualiza um ou mais recursos no back-end, como um formulário.
+// // PATCH => Atualiza uma informação específica em um recurso no back-end.
+// // DELETE => Remove um recusos do back-end.
 
-// GET/users => Busca os usuários do back-end na rota.
-// POST/users => Cria um usuário no back-end na rota.
+// // GET/users => Busca os usuários do back-end na rota.
+// // POST/users => Cria um usuário no back-end na rota.
 
-// Stateful - Aplicação que quando é reiniciada os dados não persistem.
-// Stateless - Aplicação que quando é reiniciada as informações ficam persistidas em um banco de dados.
+// // Stateful - Aplicação que quando é reiniciada os dados não persistem.
+// // Stateless - Aplicação que quando é reiniciada as informações ficam persistidas em um banco de dados.
 
-// Cabeçalhos - (Requisição/Resposta) => Metadados.
+// // Cabeçalhos - (Requisição/Resposta) => Metadados.
 
-// HTTP Status Code
+// // HTTP Status Code
+//  }
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString()) // req.body => criou uma propriedade body dentro do req. antes o body era uma const agora ficou uma propriedade dentro do req
-  } catch {
-    req.body = null
-  }
-
-
-
-  console.log(req.body);
+  await json(req, res)
 
   if (method === "GET" && url === "/users") {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
---
+++ streams/buffers.js
@@ -0,0 +1,3 @@
+const buff = Buffer.from('Hello World') // vai mostrar o conteúdo em hedadecimal.
+
+console.log(buff.toJSON()); //.toJSON demonstra em decimal.
