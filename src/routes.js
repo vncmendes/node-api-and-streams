ef168fd69555bcd98aadf01b9924c2ef89645c0c
Vinicius Mendes <viniciussmendes@gmail.com>
2023-04-05 15:39:11 +0000

    increasing features 3

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
 
@@ -29,5 +30,21 @@ export const routes = [
 
       return res.writeHead(201).end()
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
   }
+  // {
+  //   method: 'PUT',
+  //   path: buildRoutePath('/users'),
+  //   handler: (req, res) => { }
+  // }
 ]
